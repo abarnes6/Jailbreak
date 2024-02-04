815a08271c7f093c5f25e07d14ae19c92edd7db1
MSWS <imodmaker@gmail.com>
2024-02-03 17:18:01 -0800

    Start work on ST

---
+++ mod/Jailbreak.Warden/Commands/SpecialTreatmentBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using CounterStrikeSharp.API.Core;
+using Jailbreak.Public.Behaviors;
+using Jailbreak.Public.Mod.Warden;
+
+namespace Jailbreak.Warden.Commands;
+
+public class SpecialTreatmentBehavior : IPluginBehavior, ISpecialTreatmentService
+{
+    private ISet<CCSPlayerController> _sts = new HashSet<CCSPlayerController>();
+
+    public void Start(BasePlugin parent)
+    {
+        parent.RegisterEventHandler<EventPlayerDisconnect>(OnPlayerDisconnect); 
+    }
+
+    HookResult OnPlayerDisconnect(CCSPlayerController player, GameEventInfo info)
+    {
+        if(!player.IsValid)
+            return HookResult.Continue;
+        
+        return HookResult.Continue;
+    }
+
+    public bool IsSpecialTreatment(CCSPlayerController? player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetSpecialTreatment(CCSPlayerController? player, bool value)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ public/Jailbreak.Public/Mod/Warden/ISpecialTreatmentService.cs
@@ -0,0 +1,9 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace Jailbreak.Public.Mod.Warden;
+
+public interface ISpecialTreatmentService
+{
+     bool IsSpecialTreatment(CCSPlayerController? player);
+     void SetSpecialTreatment(CCSPlayerController? player, bool value);
+}
